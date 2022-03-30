d3415d4e4e31a142e359b3e1106b2ebe6805ad22
Calle Johansson <callej@live.com>
2022-03-30 09:22:00 -0700

    Update Main.kt
    Changed some names, added lines for intermediate calculations and split the lines in the enum class to make the code more readable and easier to understand and extend.

--- Main.kt
+++ Main.kt
@@ -1,24 +1,43 @@
 package converter
 
-enum class Units(val type: String, val uName: List<String>, val sing: String, val plur: String, val from: (Double) -> Double, val to: (Double) -> Double) {
-    M("Length", listOf("m", "meter", "meters"), "meter", "meters", { x -> x }, { x -> x }),
-    KM("Length", listOf("km", "kilometer", "kilometers"), "kilometer", "kilometers", { x -> 1000 * x }, { x -> x / 1000 }),
-    CM("Length", listOf("cm", "centimeter", "centimeters"), "centimeter", "centimeters", { x -> 0.01 * x }, { x -> x / 0.01 }),
-    MM("Length", listOf("mm", "millimeter", "millimeters"), "millimeter", "millimeters", { x -> 0.001 * x }, { x -> x / 0.001 }),
-    MI("Length", listOf("mi", "mile", "miles"), "mile", "miles", { x -> 1609.35 * x }, { x -> x / 1609.35 }),
-    YD("Length", listOf("yd", "yard", "yards"), "yard", "yards", { x -> 0.9144 * x }, { x -> x / 0.9144 }),
-    FT("Length", listOf("ft", "foot", "feet"), "foot", "feet", { x -> 0.3048 * x }, { x -> x / 0.3048 }),
-    IN("Length", listOf("in", "inch", "inches"), "inch", "inches", { x -> 0.0254 * x }, { x -> x / 0.0254 }),
-    G("Weight", listOf("g", "gram", "grams"), "gram", "grams", { x -> x }, { x -> x }),
-    KG("Weight", listOf("kg", "kilogram", "kilograms"), "kilogram", "kilograms", { x -> 1000 * x }, { x -> x / 1000 }),
-    MG("Weight", listOf("mg", "milligram", "milligrams"), "milligram", "milligrams", { x -> 0.001 * x }, { x -> x / 0.001 }),
-    LB("Weight", listOf("lb", "pound", "pounds"), "pound", "pounds", { x -> 453.592 * x }, { x -> x / 453.592 }),
-    OZ("Weight", listOf("oz", "ounce", "ounces"), "ounce", "ounces", { x -> 28.3495 * x }, { x -> x / 28.3495 }),
-    C("Temp", listOf("degree celsius", "degrees celsius", "celsius", "dc", "c"), "degree Celsius", "degrees Celsius", { x -> x }, { x -> x }),
-    F("Temp", listOf("degree fahrenheit", "degrees fahrenheit", "fahrenheit", "df", "f"), "degree Fahrenheit", "degrees Fahrenheit", { x -> (x - 32) * 5 / 9 }, { x -> x * 9 / 5 + 32 }),
-    K("Temp", listOf("kelvin", "kelvins", "k"), "kelvin", "kelvins", { x -> x - 273.15 }, { x -> x + 273.15 }),
-    NULL("", listOf(""), "", "", { 0.0 }, { 0.0 }),
-    ERROR("Parse error", listOf(""), "", "", { 0.0 }, { 0.0 });
+enum class Units(val type: String, val uName: List<String>,
+                 val singular: String, val plural: String, val from: (Double) -> Double, val to: (Double) -> Double) {
+    M("Length", listOf("m", "meter", "meters"),
+        "meter", "meters", { x -> x }, { x -> x }),
+    KM("Length", listOf("km", "kilometer", "kilometers"),
+        "kilometer", "kilometers", { x -> 1000 * x }, { x -> x / 1000 }),
+    CM("Length", listOf("cm", "centimeter", "centimeters"),
+        "centimeter", "centimeters", { x -> 0.01 * x }, { x -> x / 0.01 }),
+    MM("Length", listOf("mm", "millimeter", "millimeters"),
+        "millimeter", "millimeters", { x -> 0.001 * x }, { x -> x / 0.001 }),
+    MI("Length", listOf("mi", "mile", "miles"),
+        "mile", "miles", { x -> 1609.35 * x }, { x -> x / 1609.35 }),
+    YD("Length", listOf("yd", "yard", "yards"),
+        "yard", "yards", { x -> 0.9144 * x }, { x -> x / 0.9144 }),
+    FT("Length", listOf("ft", "foot", "feet"),
+        "foot", "feet", { x -> 0.3048 * x }, { x -> x / 0.3048 }),
+    IN("Length", listOf("in", "inch", "inches"),
+        "inch", "inches", { x -> 0.0254 * x }, { x -> x / 0.0254 }),
+    G("Weight", listOf("g", "gram", "grams"),
+        "gram", "grams", { x -> x }, { x -> x }),
+    KG("Weight", listOf("kg", "kilogram", "kilograms"),
+        "kilogram", "kilograms", { x -> 1000 * x }, { x -> x / 1000 }),
+    MG("Weight", listOf("mg", "milligram", "milligrams"),
+        "milligram", "milligrams", { x -> 0.001 * x }, { x -> x / 0.001 }),
+    LB("Weight", listOf("lb", "pound", "pounds"),
+        "pound", "pounds", { x -> 453.592 * x }, { x -> x / 453.592 }),
+    OZ("Weight", listOf("oz", "ounce", "ounces"),
+        "ounce", "ounces", { x -> 28.3495 * x }, { x -> x / 28.3495 }),
+    C("Temp", listOf("degree celsius", "degrees celsius", "celsius", "dc", "c"),
+        "degree Celsius", "degrees Celsius", { x -> x }, { x -> x }),
+    F("Temp", listOf("degree fahrenheit", "degrees fahrenheit", "fahrenheit", "df", "f"),
+        "degree Fahrenheit", "degrees Fahrenheit", { x -> (x - 32) * 5 / 9 }, { x -> x * 9 / 5 + 32 }),
+    K("Temp", listOf("kelvin", "kelvins", "k"),
+        "kelvin", "kelvins", { x -> x - 273.15 }, { x -> x + 273.15 }),
+    NULL("", listOf(""),
+        "", "", { 0.0 }, { 0.0 }),
+    ERROR("Parse error", listOf(""),
+        "", "", { 0.0 }, { 0.0 });
 
     companion object {
         fun contains(unitName: String): Units {
@@ -55,17 +74,22 @@ fun main() {
         print("\nEnter what you want to convert (or exit): ")
         val msg = readln().lowercase().split(" ")
         if (msg[0] == "exit") break
-        val (fU, tU) = parse(msg)
-        if (fU != Units.ERROR && Regex("[+-]?\\d+(\\.\\d+)?").matches(msg[0])) {
-            if (fU != Units.NULL && tU != Units.NULL && fU.type == tU.type) {
-                val v = msg[0].toDouble()
-                if (v < 0 && (fU.type == "Length" || fU.type == "Weight")) {
-                    println("${fU.type} shouldn't be negative")
+        val (fromUnit, toUnit) = parse(msg)
+        if (fromUnit != Units.ERROR && Regex("[+-]?\\d+(\\.\\d+)?").matches(msg[0])) {
+            if (fromUnit != Units.NULL && toUnit != Units.NULL && fromUnit.type == toUnit.type) {
+                val value = msg[0].toDouble()
+                if (value < 0 && (fromUnit.type == "Length" || fromUnit.type == "Weight")) {
+                    println("${fromUnit.type} shouldn't be negative")
                     continue
                 }
-                println("$v ${if (v == 1.0) fU.sing else fU.plur} is ${tU.to(fU.from(v))} ${if (tU.to(fU.from(v)) == 1.0) tU.sing else tU.plur}")
+                val fromUnitString = if (value == 1.0) fromUnit.singular else fromUnit.plural
+                val convertedValue = toUnit.to(fromUnit.from(value))
+                val toUnitString = if (convertedValue == 1.0) toUnit.singular else toUnit.plural
+                println("$value $fromUnitString is $convertedValue $toUnitString")
             } else {
-                println("Conversion from ${if (fU == Units.NULL) "???" else fU.plur} to ${if (tU == Units.NULL) "???" else tU.plur} is impossible")
+                val fromUnitString = if (fromUnit == Units.NULL) "???" else fromUnit.plural
+                val toUnitString = if (toUnit == Units.NULL) "???" else toUnit.plural
+                println("Conversion from $fromUnitString to $toUnitString is impossible")
             }
         } else {
             println("Parse error")
